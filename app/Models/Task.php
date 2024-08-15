3c3d28787132c07925988fd76b93e024c50a32a1
Imali Susan <susanimali52@gmail.com>
2024-08-15 19:42:42 +0300

    Add slugs to tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,15 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
     use HasFactory;
 
+    use HasSlug;
+
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
     }
 }
