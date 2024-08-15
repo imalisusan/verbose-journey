e51b7a2a0d8442ad14d6a1dc8a6c86c73ec34c71
Imali Susan <susanimali52@gmail.com>
2024-08-15 19:44:44 +0300

    Add slugs to project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,15 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
     use HasFactory;
 
+    use HasSlug;
+
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
     }
 }
