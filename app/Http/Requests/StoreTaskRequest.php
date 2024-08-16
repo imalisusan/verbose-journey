7ee5739c972175fcf3256daed8ba88e3bb651f9b
Imali Susan <susanimali52@gmail.com>
2024-08-16 13:50:05 +0300

    Add a store task request

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check(); 
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'project_id' => 'required|exists:projects,id',
+        ];
+    }
+}
